13693a4f78961832765950b623da84355206537b
duckdoe <fortunefoluso@gmail.com>
2026-01-05 13:08:24 +0100

    styling header

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from app.api import api_router
 from app.views import view_router
-from app.models import create_tables
 
 app = Flask(__name__)
 
@@ -9,5 +8,4 @@ app = Flask(__name__)
 def create_app():
     app.register_blueprint(view_router)
     app.register_blueprint(api_router)
-    create_tables()
     return app
